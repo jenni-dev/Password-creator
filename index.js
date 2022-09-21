bc5ac7c13e864153e022f1757594d3b48bf1e492
jenni-dev <79730300+jenni-dev@users.noreply.github.com>
2022-09-21 20:01:31 +1000

    add files

---
+++ index.js
@@ -0,0 +1,45 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let password1 = document.getElementById("password1")
+let password2 = document.getElementById("password2")
+let passwordLength = 10
+
+
+function getRandomCharacter() {
+    let randomChar = Math.floor (Math.random() * characters.length)  
+    return characters[randomChar]
+}
+
+console.log(getRandomCharacter())
+console.log(password1)
+
+let generatedPasswordOne = generatePw()
+
+function generatePw() {
+        
+    
+        console.log("the button is clicked!")
+        let randomPassword = ""
+        for ( i = 0; i < passwordLength; i ++) {
+            randomPassword += getRandomCharacter()
+        }
+        return randomPassword
+        }
+        
+        password1.textContent = generatedPasswordOne
+    
+
+
+    
+  
+
+    
+        
+
+
+
+
+    
+
+
